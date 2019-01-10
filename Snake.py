59971cd872a520a449858bdcc8e995f7b1d28a31
郭盼盼 <1604929196@qq.com>
2019-01-10 22:34:27 +0800

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,5 +1,24 @@
 class Snake(object):        #定义一个蛇类
-    #蛇向一个指定点移动  
+    #蛇向一个指定点移动
+    def __init__(self, grid, color = "#000000"): #采用__init__()构造方法对蛇进行初始化
+        self.grid = grid      #蛇的形状为网格
+        self.color = color    #蛇的初始化颜色
+        self.body = [(8, 11), (8, 12), (8, 13)]   #蛇的初始化长度
+        self.direction = "Up"        #定义蛇的初始行进方向向上
+        for i in self.body:       ##用for循环遍历蛇
+            self.grid.draw(i, self.color)
+
+    #这个方法用于游戏重新开始时初始化贪吃蛇的位置
+    def initial(self):
+        while not len(self.body) == 0:
+            pop = self.body.pop()
+            self.grid.draw(pop, self.grid.bg)
+        self.body = [(8, 11), (8, 12), (8, 13)]
+        self.direction = "Up"
+        self.color = "#000000"
+        for i in self.body:
+            self.grid.draw(i, self.color)
+            
     def move(self, new):    #定义move()方法使蛇发生移动
         self.body.insert(0, new)
         pop = self.body.pop()
