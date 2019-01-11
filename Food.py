52659dd0a1da0a924d19adedcdaf902c9e376ece
郭盼盼 <1604929196@qq.com>
2019-01-11 12:23:35 +0800

    Update Food.py

--- Food.py
+++ Food.py
@@ -1,15 +1,14 @@
-class Food(object):     #Food类
-    def __init__(self, grid, color = "#23D978"):
-        #初始化函数，包含网格grid和颜色color参数 
-        self.grid = grid     #把参数grid赋值给对象变量grid  
-        self.color = color   #把参数color赋值给对象变量color 
-        self.set_pos()       #食物Food的位置
-        self.type = 1        #食物Food类型1
+class Food(object):     #定义一个食物类，用于设置食物的各种参数
+    def __init__(self, grid,color="#23D978"):    #采用__init__()构造方法定义一个食物
+        self.grid = grid       #引用grid类为食物网格
+        self.color = color     #食物的颜色
+        self.set_pos()         #食物的位置
+        self.type = 1          #第一次游戏开始时食物的类型
 
-    def set_pos(self):       #定义食物出现位置的set_pos()方法
-        x = randint(0, self.grid.width - 1)   #调用randint随机出现食物横坐标的位置
-        y = randint(0, self.grid.height - 1)  #调用randint随机出现食物纵坐标的位置
-        self.pos = (x, y)
-        #返回食物出现的位置坐标
+    def set_pos(self):        #定义食物出现位置的set_pos()方法
+        x = randint(0, self.grid.width - 1)    #调用randint随机出现食物横坐标的位置
+        y = randint(0, self.grid.height - 1)   #调用randint随机出现食物纵坐标的位置
+        self.pos = (x, y)        #返回食物出现的位置坐标
+
     def display(self):       #定义display方法显示食物和蛇
         self.grid.draw(self.pos, self.color)
